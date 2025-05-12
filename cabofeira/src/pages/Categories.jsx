c889600edf6fd5c35e24d099c430ffa86dd953f2
Damian5314 <damianwillmse@gmail.com>
2025-05-12 20:04:52 +0200

    Changed way categories is imported on categories page

--- cabofeira/src/pages/Categories.jsx
+++ cabofeira/src/pages/Categories.jsx
@@ -1,33 +1,10 @@
 import React from "react";
 import "./Categories.css";
-
-const categories = {
-  "Bicycles and Mopeds": ["Aprilia", "Bicycle Accessories", "Bicycle Parts"],
-  "Cars & Trucks": ["Abarth", "Alfa Romeo", "BMW"],
-  "Clothing and Shoes": ["Clothing Children", "Clothing Gentlemen", "Clothing Ladies"],
-  "Video Games": ["Playstation", "Nintendo", "Game Accessories"],
-  // Voeg hier meer toe
-};
+import CategoryList from "../components/CategoryList";
 
 function Categories() {
   return (
-    <div className="categories-container">
-      <h1>Ad Categories</h1>
-      <div className="category-columns">
-        {Object.entries(categories).map(([group, items]) => (
-          <div key={group} className="category-group">
-            <h3>{group}</h3>
-            <ul>
-              {items.map((item) => (
-                <li key={item}>
-                  <a href="#">{item}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
-    </div>
+    <CategoryList />
   );
 }
 
