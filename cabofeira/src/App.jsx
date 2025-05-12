0c237efbcd57bbdeaa4c9435946d082d7b29aa5b
Damian5314 <damianwillmse@gmail.com>
2025-05-12 19:54:34 +0200

    categories page

--- cabofeira/src/App.jsx
+++ cabofeira/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Categories from "./pages/Categories";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
         {/* Meer routes komen hier later */}
       </Routes>
     </Router>
