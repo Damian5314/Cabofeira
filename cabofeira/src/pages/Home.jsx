de3c9aa6dfcba26a4aabdfc9136ae35255cd0476
Damian5314 <damianwillmse@gmail.com>
2025-05-12 19:51:24 +0200

    Homepage

---
+++ cabofeira/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="hero-section">
+      <div className="overlay">
+        <h1>Search for… <span className="highlight">anything</span> with CaboFeira!</h1>
+        <p>Get to know CaboFeira and be free to post anything and sell it.</p>
+        <p>Earn extra with what you no longer use.</p>
+        <div className="search-bar">
+          <input type="text" placeholder="What are you looking for?" />
+          <input type="text" placeholder="Location" />
+          <select>
+            <option>All Categories</option>
+          </select>
+          <button>Search</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
