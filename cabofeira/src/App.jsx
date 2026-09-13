375e2334a0fd1f9b74509c4a660e66ae021b08e8
Damian5314 <damianwillmse@gmail.com>
2026-09-13 13:41:33 +0200

    analytics

--- cabofeira/src/App.jsx
+++ cabofeira/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
@@ -84,6 +85,7 @@ function App() {
                   </Routes>
                   </main>
                   <Footer />
+                  <Analytics />
                 </Router>
               </MessagesProvider>
             </PricingProvider>
